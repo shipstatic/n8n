7cba3617616e0d8248e2c24682e64439b8007916
ShipStatic <hello@shipstatic.com>
2026-03-11 15:02:27 +0200

    initial commit

---
+++ credentials/ShipstaticApi.credentials.ts
@@ -0,0 +1,41 @@
+import type {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class ShipstaticApi implements ICredentialType {
+	name = 'shipstaticApi';
+	displayName = 'ShipStatic API';
+	icon = 'file:shipstatic.svg' as const;
+	documentationUrl = 'https://docs.shipstatic.com';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			typeOptions: { password: true },
+			default: '',
+			placeholder: 'ship-...',
+			description: 'Find your API key at <a href="https://my.shipstatic.com/settings">my.shipstatic.com/settings</a>',
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: '={{"Bearer " + $credentials.apiKey}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://api.shipstatic.com',
+			url: '/account',
+			method: 'GET',
+		},
+	};
+}
